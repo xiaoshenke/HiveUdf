e391a045408520229b2b7373cbed43657acca93f
xiaoshenke <1205914137@qq.com>
2017-12-25 15:04:22 +0800

    export pg

---
+++ src/main/java/wuxian/me/datetimesparksql/ExportPostgresUDF.java
@@ -0,0 +1,155 @@
+package wuxian.me.datetimesparksql;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.BasicDataSourceFactory;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.UDFType;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.ql.udf.generic.NDV;
+import org.apache.hadoop.hive.serde2.objectinspector.ConstantObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.io.BooleanWritable;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+//ref:http://blog.csdn.net/xiao_jun_0820/article/details/53404939
+
+@UDFType(deterministic = true)
+@Description(name = "export_postgres",
+        value = "_FUNC_() export data to postgres.")
+@NDV(maxNdv = 1)
+public class ExportPostgresUDF extends GenericUDF {
+
+    private String sql;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
+        if (objectInspectors.length < 3) {
+            throw new UDFArgumentException(" Expecting  at least three  arguments ");
+        }
+
+        if (objectInspectors[0].getCategory() == ObjectInspector.Category.PRIMITIVE
+                && ((PrimitiveObjectInspector) objectInspectors[0]).getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.STRING) {
+            if (!(objectInspectors[0] instanceof ConstantObjectInspector)) {
+                throw new UDFArgumentException("postgres connection url must be constant");
+            }
+            ConstantObjectInspector propertiesPath = (ConstantObjectInspector) objectInspectors[0];
+            String configPath = propertiesPath.getWritableConstantValue().toString();
+
+            parse(configPath);
+
+            Properties properties = new Properties();
+            properties.setProperty("username", user.substring("username=".length()));
+            properties.setProperty("password", password.substring("password=".length()));
+            properties.setProperty("url", url.substring("url=".length()));
+            properties.setProperty("maxActive", String.valueOf(20));
+            properties.setProperty("driverClassName", "org.postgresql.Driver");
+            try {
+                this.dataSource = BasicDataSourceFactory.createDataSource(properties);
+            } catch (Exception e) {
+                throw new UDFArgumentException("create postgres connection fail!");
+            }
+
+        }
+
+        if (objectInspectors[1].getCategory() == ObjectInspector.Category.PRIMITIVE
+                && ((PrimitiveObjectInspector) objectInspectors[1]).getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.STRING) {
+            if (!(objectInspectors[1] instanceof ConstantObjectInspector)) {
+                throw new UDFArgumentException("the second arg   must be a sql string constant");
+            }
+            ConstantObjectInspector sqlInsp = (ConstantObjectInspector) objectInspectors[1];
+            this.sql = sqlInsp.getWritableConstantValue().toString();
+            if (this.sql == null || this.sql.trim().length() == 0) {
+                throw new UDFArgumentException("the second arg   must be a sql string constant and not nullable");
+            }
+        }
+        paramsInspectors = new PrimitiveObjectInspector[objectInspectors.length - 2];
+        for (int i = 2; i < objectInspectors.length; i++) {
+            paramsInspectors[i - 2] = (PrimitiveObjectInspector) objectInspectors[i];
+        }
+
+        return PrimitiveObjectInspectorFactory.writableBooleanObjectInspector;
+    }
+
+    private String url;
+    private String user;
+    private String password;
+
+    private DataSource dataSource;
+
+    private PrimitiveObjectInspector[] paramsInspectors;
+
+    public void close() throws IOException {
+        try {
+            BasicDataSource bds = (BasicDataSource) dataSource;
+            bds.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new IOException(e);
+        }
+    }
+
+    public void parse(String configPath) throws UDFArgumentException {
+        String reg = "\\s";
+        String[] params = configPath.split(reg);
+
+        for (String param : params) {
+            if (param.startsWith("username=")) {
+                user = param;
+            } else if (param.startsWith("password=")) {
+                password = param;
+            } else if (param.startsWith("url=")) {
+                url = param;
+            }
+        }
+        if (url == null || password == null || user == null) {
+            throw new UDFArgumentException("url or password or user if empty!");
+        }
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    @Override
+    public Object evaluate(DeferredObject[] deferredObjects) throws HiveException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement stmt = connection.prepareStatement(sql)) {
+            System.out.println("execute sql:" + System.currentTimeMillis());
+            for (int i = 2; i < deferredObjects.length; i++) {
+                Object param = paramsInspectors[i - 2].getPrimitiveJavaObject(deferredObjects[i].get());
+                stmt.setObject(i - 1, param);
+            }
+            int ret = stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new HiveException(e);
+        }
+
+        return new BooleanWritable(true);
+    }
+
+    @Override
+    public String getDisplayString(String[] strings) {
+        return "EXPORT_POSTGRES";
+    }
+}
---
+++ src/test/java/wuxian/me/datetimesparksql/ExportPostgresUDFTest.java
@@ -0,0 +1,28 @@
+package wuxian.me.datetimesparksql;
+
+import org.apache.avro.TestAnnotation;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExportPostgresUDFTest {
+
+    @Test
+    public void testUDF() {
+
+        ExportPostgresUDF udf = new ExportPostgresUDF();
+
+        String orginString = "jdbc:xxxxx;user=hello;password=xxx";
+        System.out.println("origin: " + orginString);
+        try {
+            udf.parse(orginString);
+        } catch (Exception e) {
+
+        }
+
+        System.out.println("url: " + udf.getUrl());
+        System.out.println("user: " + udf.getUser());
+        System.out.println("password: " + udf.getPassword());
+    }
+
+}
